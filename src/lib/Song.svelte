0e4384be22e6175ffbe3e2ec0ad3c1fe86dca92f
ladyofcode <thelab@ladyofcode.com>
2023-12-12 20:35:21 +1100

    Add Song component

---
+++ src/lib/Song.svelte
@@ -0,0 +1,10 @@
+<div>
+    <!-- Spotify preview embed -->
+    <h3>Song title</h3>
+    <p>Artist</p>
+
+    <div>
+        <button>YouTube</button>
+        <button>UG Tabs</button>
+    </div>
+</div>
