a5018b5a46b347c1d32241accf39d08b27079710
ladyofcode <thelab@ladyofcode.com>
2024-01-13 16:17:26 +0000

    Update song component

--- src/lib/Song.svelte
+++ src/lib/Song.svelte
@@ -1,15 +1,23 @@
 <script>
-    export let title, artist;
+	export let title, artist;
+
+	import { browser } from '$app/environment';
+
+	import { extract } from '@extractus/oembed-extractor';
+	
+
+	
 </script>
 
 
-<div>
-    <!-- Spotify preview embed -->
-    <h3>{title}</h3>
-    <p>{artist}</p>
+<div class="glass">
+	
+	<h3>{title}</h3>
+	<p>{artist}</p>
 
-    <div>
-        <button>YouTube</button>
-        <button>UG Tabs</button>
-    </div>
+
+	<div>
+		<button>YouTube</button>
+		<button>UG Tabs</button>
+	</div>
 </div>
